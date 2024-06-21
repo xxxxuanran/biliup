fe7dfceabe4873b1ec30c63ca6eaed438afa8842
xxxxuanran <xxxxuanran@gmail.com>
2024-06-21 09:29:02 +0800

    dead

--- biliup/Danmaku/douyin_util/frontier_sign_test.py
+++ biliup/Danmaku/douyin_util/frontier_sign_test.py
@@ -1,6 +1,8 @@
 import execjs  # 导入 execjs 库
 import hashlib
 
+from xbogus import _0x26d461
+
 class ByteDance:
     @staticmethod
     def md5(string) -> str:
@@ -17,10 +19,14 @@ class ByteDance:
 # https://blog.csdn.net/m0_75268677/article/details/137596255
 
 def main():
-    e = "2f5d497b88accae9df4c90bcba019ffa"
-    list = ByteDance.decode(ByteDance.md5(ByteDance.decode(e)))
-    print(list)
-    if [105, 79, 195, 101, 93, 204, 30, 3, 92, 154, 42, 65, 123, 151, 206, 6] == list:
+    # e = "2f5d497b88accae9df4c90bcba019ffa"
+    # list = ByteDance.decode(ByteDance.md5(ByteDance.decode(e)))
+    # print(list)
+    # if [105, 79, 195, 101, 93, 204, 30, 3, 92, 154, 42, 65, 123, 151, 206, 6] == list:
+    #     print("true")
+    val = _0x26d461()
+    print(val)
+    if val == "14":
         print("true")
 
 if __name__ == "__main__":
