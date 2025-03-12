6f5bd554f9f9bf2ff833bcc9834778880a7c9502
xxxxuanran <xxxxuanran@gmail.com>
2025-03-12 13:04:17 +0800

    tmp

--- biliup/plugins/bilibili.py
+++ biliup/plugins/bilibili.py
@@ -1,7 +1,8 @@
 import time
 import json
 import re
-import asyncio
+
+from typing import *
 
 from biliup.common.util import client
 from biliup.config import config
@@ -25,7 +26,7 @@ class Bililive(DownloadBase):
         self.__login_mid = 0
         self.bili_cookie = config.get('user', {}).get('bili_cookie')
         self.bili_cookie_file = config.get('user', {}).get('bili_cookie_file')
-        self.bili_qn = int(config.get('bili_qn', 20000))
+        self.bili_qn = -1
         self.bili_protocol = config.get('bili_protocol', 'stream')
         self.bili_cdn = config.get('bili_cdn', [])
         self.bili_hls_timeout = config.get('bili_hls_transcode_timeout', 60)
@@ -93,31 +94,41 @@ class Bililive(DownloadBase):
         self.__real_room_id = room_info['room_info']['room_id']
         live_start_time = room_info['room_info']['live_start_time']
         special_type = room_info['room_info']['special_type'] # 0: 公开直播, 1: 大航海专属
-        if live_start_time > self.live_start_time:
-            self.live_start_time = live_start_time
-            is_new_live = True
-        else:
-            is_new_live = False
 
         if is_check:
             return True
-        else:
+
+        if self.__login_mid == 0:
             self.__login_mid = await self.check_login_status()
 
         # 复用原画 m3u8 流
         if  self.raw_stream_url is not None \
             and ".m3u8" in self.raw_stream_url \
-            and self.bili_qn >= 10000 \
-            and not is_new_live:
+            and self.bili_qn >= 10000:
             url = await self.acheck_url_healthy(self.raw_stream_url)
             if url is not None:
-                logger.debug(f"{self.plugin_msg}: 复用 {url}")
+                logger.info(f"{self.plugin_msg}: 复用 {url}")
                 return True
-            else:
-                self.raw_stream_url = None
+            self.raw_stream_url = None
 
+        qn_with_codec = {}
+        if self.bili_qn == -1:
+            accept_qn = config.get('bili_qn', ['10000', 'avc10000', 'hevc10000'])
+            accept_qn = ['hevc400', '10000', 'avc10000', 'hevc10000']
+            if isinstance(accept_qn, int):
+                # 旧配置的选项值
+                accept_qn = [str(accept_qn)]
+            elif isinstance(accept_qn, str):
+                # 旧配置的自定义值
+                accept_qn = [accept_qn]
+            for qn in accept_qn:
+                qn_str = match1(qn, r'(\d+)')
+                codec = qn.split(qn_str)[0]
+                if not codec:
+                    codec = 'any'
+                qn_with_codec.setdefault(int(qn_str), []).append(codec)
 
-        stream_urls = await self.aget_stream(self.bili_qn, self.bili_protocol, special_type)
+        stream_urls = await self.aget_stream(qn_with_codec, self.bili_protocol, special_type)
         if not stream_urls:
             if self.bili_protocol == 'hls_fmp4':
                 if int(time.time()) - live_start_time <= self.bili_hls_timeout:
@@ -223,82 +234,102 @@ class Bililive(DownloadBase):
         params = {
             'room_id': self.__real_room_id,
             'protocol': '0,1',  # 流协议，0: http_stream(flv), 1: http_hls
-            'format': '0,1,2',  # 编码格式，0: flv, 1: ts, 2: fmp4
-            'codec': '0',  # 编码器，0: avc, 1: hevc, 2: av1
+            'format': '0,1,2',  # 格式，0: flv, 1: ts, 2: fmp4
+            'codec': '0,1',  # 编码器，0: avc, 1: hevc, 2: av1
             'qn': qn,
             'platform': 'html5',  # 平台名称，web, html5, android, ios
             # 'ptype': '8', # P2P配置，-1: disable, 8: WebRTC, 8192: MisakaTunnel
-            'dolby': '5', # 杜比格式，5: 杜比音频
+            # 'dolby': '5', # 杜比
             # 'panorama': '1', # 全景(不支持 html5)
-            # 'http': '1', # 优先 http 协议
+            # 'hdr_type': '0,1', # HDR类型(不支持 html5)，0: SDR, 1: PQ
+            # 'req_reason': '0', # 请求原因，0: Normal, 1: PlayError
+            # 'http': '1', # 直播流是否使用 http
         }
         try:
-            api_res = await client.get(
+            res = await client.get(
                 full_url, params=params, headers=self.fake_headers
             )
-            api_res = json.loads(api_res.text)
-            if api_res['code'] != 0:
-                logger.error(f"{self.plugin_msg}: {api} 返回内容错误: {api_res}")
-                return {}
-            return api_res['data']
+            res = json.loads(res.text)
+            return check_areablock(res)
         except json.JSONDecodeError:
-            logger.error(f"{self.plugin_msg}: {api} 返回内容错误: {api_res.text}")
+            logger.error(f"{self.plugin_msg}: {api} 返回内容错误: {res.text}")
         except Exception as e:
             logger.error(f"{self.plugin_msg}: {api} 获取 play_info 失败 -> {e}", exc_info=True)
         return {}
 
     async def get_master_m3u8(self, api: str) -> dict:
-        full_url = f"{api}/live-bvc/master.m3u8"
+        '''
+        Web端首屏用的是 API，
+        只有 API 存在 master_url 才会添加画质选项 -1(自动) 并隐式切换
+        '''
+        full_url = f"{api}/xlive/web-room/v2/index/master_playurl"
         params = {
             "cid": self.__real_room_id,
-            "mid": self.__login_mid,
+            "mid": self.__login_mid, # 若为有效用户之uid，将按qn降序排列流，否则按qn升序排列
             "pt": "html5", # platform
-            # "p2p_type": "-1",
+            "p2p_type": "-1", # -1 禁用，1 启用
+            "net": "0", # 0 非无线网络
+            "free_type": "0",
+            "build": "0",
+            # "stream_name": "", # 筛选流编码
         }
         try:
-            m3u8_res = await client.get(
+            res = await client.get(
                 full_url, params=params, headers=self.fake_headers
             )
-            if m3u8_res.status_code == 200 and m3u8_res.text.startswith("#EXTM3U"):
-                return self.parse_master_m3u8(m3u8_res.text)
+            if res.status_code != 200:
+                raise Exception(f"Status Code: {res.status_code}")
+            if not res.text.startswith("#EXTM3U"):
+                raise ValueError(f"{res.text}")
+            return self.parse_master_m3u8(res.text)
         except Exception as e:
-            logger.error(f"{self.plugin_msg}: {api} 获取 m3u8 失败 -> {e}", exc_info=True)
+            logger.error(f"{self.plugin_msg}: {api} 获取 m3u8 失败 -> {e}")
         return {}
 
-    async def aget_stream(self, qn: int = 10000, protocol: str = 'stream', special_type: int = 0) -> dict:
+    async def aget_stream(self, qn_with_codec: dict, protocol: str = 'stream', special_type: int = 0) -> dict:
         """
-        :param qn: 目标画质
+        :param qn_with_codec: 用户接受的 qn 与 codec
         :param protocol: 流协议
         :param special_type: 特殊直播类型
         :return: 流信息
         """
         stream_urls = {}
+        play_info = {}
         for api in self.bili_api_list:
-            play_info = await self.get_play_info(api, qn)
-            if not play_info or check_areablock(play_info):
-                # logger.error(f"{self.plugin_msg}: {api} 返回内容错误: {play_info}")
+            '''
+            一般用户配置均为原画，只传首个 qn 可减少请求次数
+            在只有原画的流中，即使请求其他 qn，返回的流信息也是原画
+            '''
+            play_info = await self.get_play_info(api, list(qn_with_codec.keys())[0])
+            if not play_info:
                 continue
-            streams = play_info['playurl_info']['playurl']['stream']
+            if not play_info.get('pwd_verified', True):
+                # 无解说声音的赛事转播清流等
+                raise Exception("暂不支持密码保护的直播间")
+            streams = play_info['playurl_info'].get('playurl', {}).get('stream')
+            if not streams:
+                continue
             if protocol == 'hls_fmp4':
                 if self.bili_anonymous_origin:
                     if special_type in play_info['all_special_types'] and not self.__login_mid:
-                        logger.warn(f"{self.plugin_msg}: 特殊直播{special_type}")
+                        logger.warn(f"{self.plugin_msg}: 特殊直播 {special_type}")
                     else:
                         stream_urls = await self.get_master_m3u8(api)
-                        if stream_urls:
-                            break
+                        if not stream_urls:
+                            continue
+                        break
                 # 处理 API 信息
                 stream = streams[1] if len(streams) > 1 else streams[0]
                 for format in stream['format']:
                     if format['format_name'] == 'fmp4':
-                        stream_urls = self.parse_stream_url(format['codec'][0])
+                        stream_urls = self.parse_stream_url(format['codec'])
                         # fmp4 可能没有原画
                         if qn == 10000 and qn in stream_urls.keys():
                             break
                         else:
                             stream_urls = {}
             else:
-                stream_urls = self.parse_stream_url(streams[0]['format'][0]['codec'][0])
+                stream_urls = self.parse_stream_url(streams[0]['format'][0]['codec'])
             if stream_urls:
                 break
         # 空字典照常返回，重试交给上层方法处理
@@ -310,10 +341,13 @@ class Bililive(DownloadBase):
         :return: 当前登录用户 mid
         """
         try:
-            _res = await client.get('https://api.bilibili.com/x/web-interface/nav', headers=self.fake_headers)
+            _res = await client.get(
+                'https://api.bilibili.com/x/web-interface/nav',
+                headers=self.fake_headers
+            )
             user_data = json.loads(_res.text).get('data', {})
             if user_data.get('isLogin'):
-                logger.info(f"用户名：{user_data['uname']}, mid：{user_data['mid']}, isLogin：{user_data['isLogin']}")
+                logger.info(f"当前录制用户: {user_data['uname']}")
                 return user_data['mid']
             else:
                 logger.warning(f"{self.plugin_msg}: 未登录，或将只能录制到最低画质。")
@@ -326,13 +360,18 @@ class Bililive(DownloadBase):
             return re.sub(r"(?<=cn-gotcha204)-[1-4]", "", url, 1)
         return url
 
+    def ov2cn(self, host: str) -> str:
+        if self.bili_ov2cn:
+            return host.replace("ov-", "cn-")
+        return host
+
     def parse_stream_url(self, *args) -> dict:
         suffix_regexp = r'suffix=([^&]+)'
         if isinstance(args[0], str):
             url = args[0]
             host = "https://" + match1(url, r'https?://([^/]+)')
             stream_url = {
-                'host': host if not self.bili_ov2cn else host.replace("ov-", "cn-"),
+                'host': self.ov2cn(host),
                 'base_url': url.split("?")[0].split(host)[1] + "?",
                 'extra': url.split("?")[1]
             }
@@ -341,32 +380,37 @@ class Bililive(DownloadBase):
                 'stream_name': match1(url, STREAM_NAME_REGEXP),
                 'suffix': match1(url, suffix_regexp)
             }
-        elif isinstance(args[0], dict):
+        elif isinstance(args[0], list):
+            multi_codec_streams = args[0]
             streams = {}
-            current_qn = args[0]['current_qn']
-            streams.setdefault(current_qn, {})
-            base_url = args[0]['base_url']
-            for info in args[0]['url_info']:
-                cdn_name = match1(info['extra'], r'cdn=([^&]+)')
-                stream_url = {
-                    'host': info['host'] if not self.bili_ov2cn else info['host'].replace("ov-", "cn-"),
+            for codec in multi_codec_streams:
+                cur_codec: str = codec['codec_name']
+                cur_qn: int = codec['current_qn']
+                base_url: str = codec['base_url']
+                # 先确定画质再确定编码，更方便回退到指定画质的其他编码流
+                streams.setdefault(cur_qn, { cur_codec: {} })
+                streams[cur_qn][cur_codec] = {
                     'base_url': base_url,
-                    'extra': info['extra']
-                }
-                streams[current_qn].setdefault(cdn_name, {
-                    'url': stream_url,
                     'stream_name': match1(base_url, STREAM_NAME_REGEXP),
-                    'suffix': match1(info['extra'], suffix_regexp)
-                })
-            return streams
-
+                }
+                for info in codec['url_info']:
+                    if "suffix" not in streams[cur_qn][cur_codec]:
+                        suffix: str = match1(info['extra'], suffix_regexp)
+                        streams[cur_qn][cur_codec]['suffix'] = suffix
+                    cdn_name: str = match1(info['extra'], r'cdn=([^&]+)')
+                    cdn_info = {
+                        'host': self.ov2cn(info['host']),
+                        'extra': info['extra']
+                    }
+                    streams[cur_qn][cur_codec][cdn_name] = cdn_info
+        return streams
 
     def parse_master_m3u8(self, m3u8_content: str) -> dict:
         """
         Returns:
             {
-                "qn值": {
-                    "cdn名称": {
+                "code_with_qn": {
+                    "cdn_name": {
                         "url": parsed_stream_url,
                         "stream_name": "流名称",
                         "suffix": "二压后缀"
@@ -375,40 +419,32 @@ class Bililive(DownloadBase):
             }
         """
         lines = m3u8_content.strip().splitlines()
-        current_qn = None
+        cur_qn = None
         result = {}
-
         if not lines[0].startswith('#EXTM3U'):
             raise ValueError('Invalid m3u8 file')
-
         for line in lines:
             if line.startswith('#EXT-X-STREAM-INF:'):
                 codec = match1(line, r'CODECS="([^"]+)"')
-                current_qn = match1(line, r'BILI-QN=(\d+)')
+                cur_qn = match1(line, r'BILI-QN=(\d+)')
 
-                if codec and current_qn:
-                    if 'avc' in codec.lower():
-                        result.setdefault(current_qn, {})
-                    else:
-                        current_qn = None
-
-            elif line.startswith('http') and current_qn is not None:
+            elif line.startswith('http') and cur_qn is not None:
                 cdn_name = match1(line, r'cdn=([^&]+)')
                 if cdn_name:
-                    result[current_qn].setdefault(cdn_name, self.parse_stream_url(line))
+                    result[cur_qn][cdn_name] = self.parse_stream_url(line)
 
         return dict(sorted(result.items(), key=lambda x: int(x[0]), reverse=True))
 
 # Copy from room-player.js
-def check_areablock(data):
+def check_areablock(data) -> dict:
     '''
-    :return: True if area block
+    :return: data['data']
     '''
-    if not data['playurl_info']['playurl']:
+    if data['code'] == 60005:
         logger.error('Sorry, bilibili is currently not available in your country according to copyright restrictions.')
         logger.error('非常抱歉，根据版权方要求，您所在的地区无法观看本直播')
-        return True
-    return False
+        raise Exception(f'地区限制 {data["message"]}')
+    return data['data']
 
 def normalize_url(url: str) -> str:
     return url if url.startswith(('http://', 'https://')) else 'http://' + url
